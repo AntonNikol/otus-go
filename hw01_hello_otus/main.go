70296a78eb0f4fd805c078f0b312936184d041db
Anton Nikolaev <migufa@mail.ru>
2023-06-05 01:35:56 +0500

    hw01

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
+	fmt.Println(stringutil.Reverse("Hello, gci write -s standard -s default --skip-generated main.go\nOTUS!"))
 }
