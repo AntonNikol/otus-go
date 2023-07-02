122ad823f0db30f76769b2cef592f9faa80a6c34
Anton Nikolaev <migufa@mail.ru>
2023-07-02 21:48:40 +0500

    Фиксы после ревью

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/AntonNikol/hw02_unpack_string
+module github.com/AntonNikol/otus-go/hw02_unpack_string
 
 go 1.19
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -14,12 +14,14 @@ func Unpack(str string) (string, error) {
 	var b strings.Builder
 	runes := []rune(str)
 
+	// Если 1 символ число
+	if len(str) > 0 && unicode.IsDigit(runes[0]) {
+		return "", ErrInvalidString
+	}
+
 	for i, v := range runes {
 		switch {
-		// Если 1 символ число
-		case i == 0 && unicode.IsDigit(v):
-			return "", ErrInvalidString
-			// Если следующий символ число
+		// Если следующий символ число
 		case i+1 < len(runes) && unicode.IsDigit(runes[i+1]):
 			if unicode.IsDigit(runes[i]) {
 				return "", ErrInvalidString
@@ -27,12 +29,11 @@ func Unpack(str string) (string, error) {
 			count, _ := strconv.Atoi(string(runes[i+1]))
 			b.WriteString(strings.Repeat(string(v), count))
 			continue
-
 			// Если текущий символ число
 		case unicode.IsDigit(v):
 			continue
 		default:
-			b.WriteString(string(v))
+			b.WriteRune(v)
 		}
 	}
 	return b.String(), nil
