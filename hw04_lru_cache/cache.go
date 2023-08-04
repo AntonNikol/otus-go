b61a94cf8b3e0dbce0856dd75d4c94f4d4c00971
Anton Nikolaev <migufa@mail.ru>
2023-08-04 17:37:46 +0500

    задание 4

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "fmt"
+
 type Key string
 
 type Cache interface {
@@ -22,4 +24,40 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		value := item.Value
+		fmt.Println(value)
+		return item.Value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		item.Value = value
+		return true
+	}
+
+	if c.queue.Len() >= c.capacity {
+		old := c.queue.Back()
+		if old != nil {
+			delete(c.items, old.Value.(Key))
+			c.queue.Remove(old)
+		}
+	}
+
+	item := c.queue.PushFront(value)
+	c.items[key] = item
+
+	return false
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,103 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head *ListItem
+	tail *ListItem
+	len  int
 }
 
 func NewList() List {
-	return new(list)
+	return &list{}
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	var node *ListItem
+
+	// если список пустой
+	if l.head == nil {
+		node = &ListItem{Value: v}
+		l.head = node
+		l.tail = node
+	} else {
+		// вставка в начало
+		node = &ListItem{Value: v, Next: l.head}
+		l.head.Prev = node
+		l.head = node
+	}
+	l.len++
+	return node
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	var node *ListItem
+
+	// если список пустой
+	if l.tail == nil {
+		node := &ListItem{Value: v}
+		l.head = node
+		l.tail = node
+	} else {
+		// вставка в конец
+		node := &ListItem{Value: v, Prev: l.tail}
+		l.tail.Next = node
+		l.tail = node
+	}
+	l.len++
+	return node
+}
+
+func (l *list) Remove(i *ListItem) {
+	// если 1 элемент в списке
+	if l.head == l.tail {
+		l.head = nil
+		l.tail = nil
+	} else {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+	}
+	i.Prev = nil
+	i.Next = nil
+
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	// если 1 элемент в списке
+	if l.head == i {
+		return
+	}
+
+	next := i.Next
+	prev := i.Prev
+
+	// обновляем связи для элемента i
+	if next != nil {
+		next.Prev = prev
+	}
+	if prev != nil {
+		prev.Next = next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	// перемещаем элемент в начало
+	i.Prev = nil
+	l.head = i
 }
