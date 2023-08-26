08863a20d951bbf30817b2fec0f6cc1be6626c9a
Anton Nikolaev <migufa@mail.ru>
2023-08-26 23:31:57 +0500

    Разработка метода обработки пайплайна

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,36 @@ type (
 
 type Stage func(in In) (out Out)
 
+// ExecutePipeline Выполняет пайплайн
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		if stage != nil {
+			stageCh := createStageChannel(in, done)
+			in = stage(stageCh)
+		}
+	}
+	return in
+}
+
+// createStageChannel Создает канал для передачи данных между стейджами
+func createStageChannel(in In, done In) Out {
+	stageCh := make(Bi)
+	go func() {
+		defer close(stageCh)
+		for {
+			select {
+			// если получили сигнал о завершении
+			case <-done:
+				return
+			// если канал закрыт
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				// передаем данные в канал
+				stageCh <- v
+			}
+		}
+	}()
+	return stageCh
 }
